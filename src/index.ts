9af2f38e7ccc8ddf83ad2877cf91d86a80fa2d71
Jeremy Purser <jeremypurser@Jeremys-MacBook-Pro.local>
2020-05-20 15:05:56 -0400

    Correct static path

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,32 @@ import React from 'react';
 import { renderToString } from 'react-dom/server';
 import Home from './client/components/Home';
 import config from './config';
+import path from 'path';
 
 const app = express();
 
+// Serve static assets
+app.use(express.static('public'));
+
+// Render React app server side
 app.get('/', (req, res) => {
-  const content = renderToString(React.createElement(Home));
-  res.send(content);
+  // send html document with rendered App
+  res.send(`
+    <!DOCTYPE html>
+      <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Document</title>
+        </head>
+        <body>
+          <div id="app">
+            ${renderToString(React.createElement(Home))}
+           </div>
+           <script async defer src="bundle.js"></script>
+        </body>
+      </html>
+  `);
 });
 
 app.listen(config.port, () => {
