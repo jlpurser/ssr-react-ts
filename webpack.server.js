2ba90586c5c6773794d4a298acc2a1ed2d662514
Jeremy Purser <jeremypurser@Jeremys-MacBook-Pro.local>
2020-05-20 13:49:36 -0400

    Remove empty lines

--- webpack.server.js
+++ webpack.server.js
@@ -3,21 +3,17 @@ import path from 'path';
 export default {
   // bundle for node.js
   target: 'node',
-
   // root file of render server
   entry: './src/index.ts',
-
   // location of build file
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'build'),
   },
-
   // resolver for filt paths
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
   },
-
   // run loader on every file
   module: {
     rules: [
