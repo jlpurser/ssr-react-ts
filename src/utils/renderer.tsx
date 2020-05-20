a511a207653f8a82550c84384738a24a0e2eeb07
Jeremy Purser <jeremy.purser@smashingboxes.com>
2020-05-20 16:46:48 -0400

    Add helper for rendering html

---
+++ src/utils/renderer.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { renderToString } from 'react-dom/server';
+
+export default function renderer(Component: React.ComponentType): string {
+  return `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>SSR React App</title>
+      </head>
+      <body>
+        <div id="app">${renderToString(React.createElement(Component))}</div>
+        <script async defer src="bundle.js"></script>
+      </body>
+    </html>
+  `;
+}
