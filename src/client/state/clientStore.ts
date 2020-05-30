0114c1a68f86450bac06069b8af1347220aad063
Jeremy Purser <jeremy.purser@smashingboxes.com>
2020-05-30 11:16:22 -0400

    Add arguments to configureStore

--- src/client/state/clientStore.ts
+++ src/client/state/clientStore.ts
@@ -1,10 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 import users from './usersSlice';
 
 const store = configureStore({
   reducer: {
     users: users.reducer,
   },
+  middleware: [...getDefaultMiddleware()],
+  devTools: true,
+  preloadedState: window.INITIAL_STATE,
 });
 
 export default store;
