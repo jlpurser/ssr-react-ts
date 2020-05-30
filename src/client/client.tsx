e853adcac7c00f4a25ce82cb65d38edf15ca192f
Jeremy Purser <jeremy.purser@smashingboxes.com>
2020-05-30 13:05:30 -0400

    Use render routes helper

--- src/client/client.tsx
+++ src/client/client.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import { hydrate } from 'react-dom';
 import { Provider } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
 import { BrowserRouter } from 'react-router-dom';
 import Routes from './components/Routes';
 import store from './state/clientStore';
 
 hydrate(
   <Provider store={store}>
-    <BrowserRouter>
-      <Routes />
-    </BrowserRouter>
+    <BrowserRouter>{renderRoutes(Routes)}</BrowserRouter>
   </Provider>,
   document.querySelector('#app')
 );
