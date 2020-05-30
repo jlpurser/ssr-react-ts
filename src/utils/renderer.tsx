4f3b5962782c60b1c1bd03d2d18af5019d8c4c2d
Jeremy Purser <jeremy.purser@smashingboxes.com>
2020-05-30 13:09:42 -0400

    Use render routes and attach initial state to window object

--- src/utils/renderer.tsx
+++ src/utils/renderer.tsx
@@ -2,9 +2,11 @@ import { Request } from 'express';
 import React from 'react';
 import { renderToString } from 'react-dom/server';
 import { Provider } from 'react-redux';
+import { renderRoutes } from 'react-router-config';
 import { StaticRouter } from 'react-router-dom';
+import serialize from 'serialize-javascript';
 import Routes from '../client/components/Routes';
-import { ServerStore } from '../server/state/createServerStore';
+import { ServerStore } from '../server/state/serverStore';
 
 export default function renderer(req: Request, store: ServerStore): string {
   return `
@@ -18,11 +20,12 @@ export default function renderer(req: Request, store: ServerStore): string {
       <body>
         <div id="app">${renderToString(
           <Provider store={store}>
-            <StaticRouter location={req.route.path} context={{}}>
-              <Routes />
+            <StaticRouter location={req.path} context={{}}>
+              {renderRoutes(Routes)}
             </StaticRouter>
           </Provider>
         )}</div>
+        <script>window.INITIAL_STATE = ${serialize(store.getState())}</script>
         <script async defer src="bundle.js"></script>
       </body>
     </html>
